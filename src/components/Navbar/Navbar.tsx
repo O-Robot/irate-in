c1a2c4bdfd1544426b3dd88c0c58a3ca13c2867f
O-Robot <adewaleogooluwani@gmail.com>
2024-01-22 23:36:40 +0100

    Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Logo from "../Logo/Logo";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <header className="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-[48] w-full bg-white border-b border-[#F2F2F2] text-sm py-2.5 sm:py-4 lg:ps-64 ">
+      <nav
+        className="flex basis-full items-center w-full mx-auto px-4 sm:px-6 md:px-8"
+        aria-label="Global"
+      >
+        <div className="me-5 lg:me-0 lg:hidden">
+          <button type="button" className="text-blue-500">
+            <span className="sr-only">Toggle Navigation</span>
+            <FontAwesomeIcon icon={faBars} size="lg" />
+          </button>
+        </div>
+
+        <div className="w-full flex items-center justify-end ms-auto sm:justify-between sm:gap-x-3 sm:order-3">
+          <div className="hidden sm:block">
+            <div className="relative">
+              <button className="py-2 px-4  block w-full border-gray-200 rounded-lg text-sm bg-[#0B468C] text-white ">
+                <FontAwesomeIcon icon={faAdd} className="pr-2" />
+                Add a Person
+              </button>
+            </div>
+          </div>
+
+          <div className="flex flex-row items-center justify-end gap-2">
+            <button
+              type="button"
+              className="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-[#E0E0E0] text-gray-800 hover:bg-gray-100 "
+            >
+              <FontAwesomeIcon icon={faBell} className="fa-lg  " />
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,44 +4,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 import home from "../../assets/home.png";
 import Logo from "../Logo/Logo";
-import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleRight,
+  faArrowRightFromBracket,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import Navbar from "../Navbar/Navbar";
 
 const Sidebar = () => {
   return (
-    <div className=" hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 border-e border-gray-200 pt-7 pb-10 overflow-y-none lg:block lg:translate-x-0 bg-[#0B468C] h-[100vh]">
-      {/* Logo  */}
-      <div className="px-6">
-        <Logo colour={"white"} />
+    <section>
+      <Navbar />
+
+      <div className=" hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 border-e border-gray-200 pt-7 pb-10 overflow-y-none lg:block lg:translate-x-0 bg-[#0B468C] h-[100vh]">
+        {/* Logo  */}
+        <div className="px-6">
+          <Logo colour={"white"} />
+        </div>
+        {/* Logo  */}
+        <nav className="p-6 mt-5 w-full flex flex-col flex-wrap h-[80%]">
+          <ul className="space-y-1.5">
+            <li className="bg-slate-300/10">
+              <a
+                className="flex items-center gap-x-3.5 py-4 px-5 text-sm text-white "
+                href="#"
+              >
+                <FontAwesomeIcon icon={faComments} size="lg" />
+                Messages
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="p-6">
+          <button
+            type="submit"
+            onClick={() => alert("Coming Soon!")}
+            className="flex w-full border-none justify-center rounded-md px-3 py-3 text-sm text-left bg-[#FFEBEB]  text-[#FF0000]"
+          >
+            <FontAwesomeIcon
+              icon={faArrowRightFromBracket}
+              className="mr-4 fa-lg fa-rotate-180"
+              size="lg"
+            />
+            Log out
+          </button>
+        </div>
       </div>
-      {/* Logo  */}
-      <nav className="p-6 mt-5 w-full flex flex-col flex-wrap h-[80%]">
-        <ul className="space-y-1.5">
-          <li className="bg-slate-300/10">
-            <a
-              className="flex items-center gap-x-3.5 py-4 px-5 text-sm text-white "
-              href="#"
-            >
-              <FontAwesomeIcon icon={faComments} size="lg" />
-              Messages
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <div className="p-6">
-        <button
-          type="submit"
-          onClick={() => alert("Coming Soon!")}
-          className="flex w-full justify-center rounded-md px-3 py-3 text-sm text-left bg-[#FFEBEB] border-gray-100 text-[#FF0000]"
-        >
-          <FontAwesomeIcon
-            icon={faArrowRightFromBracket}
-            className="mr-4 fa-lg fa-rotate-180"
-            size="lg"
-          />
-          Log out
-        </button>
-      </div>
-    </div>
+    </section>
   );
 };
 
