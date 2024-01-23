f99431209c316f0ecda243da875846f8e6011b34
O-Robot <adewaleogooluwani@gmail.com>
2024-01-23 19:23:16 +0100

    Navbar Profile Icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../Logo/Logo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAdd, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faAdd, faAngleDown, faBars } from "@fortawesome/free-solid-svg-icons";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 
 const Navbar = () => {
@@ -31,10 +31,22 @@ const Navbar = () => {
           <div className="flex flex-row items-center justify-end gap-2">
             <button
               type="button"
-              className="w-[2.375rem] h-[2.375rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-[#E0E0E0] text-gray-800 hover:bg-gray-100 "
+              className="w-[3rem] h-[3rem] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-[#E0E0E0] text-gray-800 hover:bg-gray-100 "
             >
               <FontAwesomeIcon icon={faBell} className="fa-lg  " />
             </button>
+            <div className="w-auto h-[3rem] inline-flex border rounded-lg border-[#E0E0E0] p-2 justify-center items-center gap-3">
+              <img
+                className="inline-block h-[32px] w-[32px] rounded-full"
+                src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                alt="Image Description"
+              />
+              <p className="text-[#4F4F4F]">John Doe </p>
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                className="text-[#E0E0E0] fa-lg"
+              />
+            </div>
           </div>
         </div>
       </nav>
