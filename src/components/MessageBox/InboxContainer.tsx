592fa182d012ff2426b1cd894fb6be7ad2956329
O-Robot <adewaleogooluwani@gmail.com>
2024-01-23 20:59:45 +0100

    empty messages container

---
+++ src/components/MessageBox/InboxContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InboxContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InboxContainer
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import EmptyMessages from "../../components/MessageBox/EmptyMessages";
 
 const Dashboard = () => {
   return (
-    <section >
+    <section>
       <Sidebar />
+      <EmptyMessages />
     </section>
   );
 };
